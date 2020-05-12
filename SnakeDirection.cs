8dbf9277caeedec7e8cce52047c1eb73d12945cf
langzihuameng <63198582+langzihuameng@users.noreply.github.com>
2020-05-13 00:38:10 +0800

    Create SnakeDirection.cs
    Snake orientation enumeration class

---
+++ SnakeDirection.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GluttonousSnake
+{
+    public enum SnakeDirection
+    {
+        Left,
+        Right,
+        Up,
+        Down
+    }
+}
