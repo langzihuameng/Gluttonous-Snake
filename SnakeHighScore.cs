c8991fbe6d068724808f0b4ccbccd32fe9aa2a07
langzihuameng <63198582+langzihuameng@users.noreply.github.com>
2020-05-13 14:39:24 +0800

    Create SnakeHighScore.cs
    High score data

---
+++ SnakeHighScore.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GluttonousSnake
+{
+    public class SnakeHighScore
+    {
+        public String PlayerName { get; set; }
+        public int Score { get; set; }
+    }
+}
